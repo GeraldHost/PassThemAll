e7fdf124fdbb0055287c94f4150672916907ca27
Gerald <me@jacobford.co.uk>
2019-06-25 06:30:35 +0100

    added colour divider tests and solution

--- dividing-colours/index.js
+++ dividing-colours/index.js
@@ -1,5 +1,29 @@
+/**
+ * 
+ * @param {int} num 
+ * 
+ * 1.1) If the number is dividable by 2 return 'red';
+ * 1.2) If the number is dividable by 5 return 'blue';
+ * 1.3) If the number is dividable by 11 return 'green';
+ * 1.4) If the number is dividable by 2 and 5 return 'pink';
+ * 1.5) If the number is dividable by 2 and 11 return 'orange';
+ * 1.6) If the number is dividable by 5 and 11 return 'black';
+ * 1.7) If the number is dividable by 5, 2 and 11 return 'yellow';
+ */
 const colourDivider = function (num) {
-
+    let map = {
+        2:  'red', // divide by 2
+        5:  'blue', // divide by 5
+        11: 'green', // divide by 11
+        7:  'pink', // divide by 2 and 5
+        13: 'orange', // divide by 2 and 11
+        16: 'black', // divide by 5 and 11
+        18: 'yellow' // divide by all three
+    };
+    let out = [2, 5, 11].reduce((count, value) => {
+        return !(num % value) ? count + value : count;
+    }, 0);
+    return map[out] || 'nope, not here';
 }
 
 module.exports = { colourDivider };
---
+++ dividing-colours/index.test.js
@@ -0,0 +1,22 @@
+const { colourDivider } = require('./index.js');
+
+each([
+    [1, 'nope, not here'],
+    [2, 'red'],
+    [5, 'blue'],
+    [11, 'green'],
+    [13, 'nope, not here'],
+    [15, 'blue'],
+    [26, 'red'],
+    [77, 'green'],
+    [80, 'pink'],
+    [91, 'nope, not here'],
+    [98, 'red'],
+    [108, 'red'],
+    [428, 'red'],
+    [821, 'nope, not here'],
+    [1021, 'nope, not here'],
+    [92001421, 'nope, not here'],
+]).test('Passing %d should return %s', (num, expected) => {
+    expect(colourDivider(num)).toBe(expected);
+});
